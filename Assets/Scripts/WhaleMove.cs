510f42e9ee21d944e0a02017e149b2be5e5aaef6
glibjibb <glibjibb@gmail.com>
2017-02-21 11:58:25 -0500

    Whale camera movement
    Whale now follows center of camera

---
+++ Assets/Scripts/WhaleMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleMove : MonoBehaviour {
+
+	public Transform target;
+	public float moveSpeed = 5.0f;
+	public float rotationSpeed = 2.5f;
+	public float centeringDistance = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveTowardsTarget ();
+	}
+
+	void MoveTowardsTarget() {
+		Vector3 direction = (target.position - transform.position);
+		Quaternion lookRotation;
+		if (direction.magnitude >= centeringDistance) {
+			lookRotation = Quaternion.LookRotation (direction);
+		} else {
+			lookRotation = target.rotation;
+		}
+		transform.rotation = Quaternion.Slerp (transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+		float step = moveSpeed * Time.deltaTime;
+		this.transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+	}
+
+}
