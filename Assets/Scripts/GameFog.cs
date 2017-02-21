b9c4276ae97926dd37891ee0cdde8e784291fa62
Spencer Franklin <franklin.sp@husky.neu.edu>
2017-02-21 12:58:42 -0500

    fog script

---
+++ Assets/Scripts/GameFog.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFog : MonoBehaviour {
+
+
+	public float waterLevel;
+	private Color underwaterColor;
+	private Color abovewaterColor;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		abovewaterColor = new Color (1f, 1f, 1f, 1f);
+		underwaterColor = new Color (0f, (15f / 255f), (163f / 255f), 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (player.transform.position.y < waterLevel) {
+
+			RenderSettings.fogColor = underwaterColor;
+			RenderSettings.fogDensity = 0.01f;
+		
+		} else {
+			
+			RenderSettings.fogColor = abovewaterColor;
+			RenderSettings.fogDensity = 0.01f;
+		}
+	}
+}
