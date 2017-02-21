a5242c168a89eaa9858482fad785366bea1403b8
glibjibb <glibjibb@gmail.com>
2017-02-21 12:20:23 -0500

    Changes to whale movement script and whale prefab

--- Assets/Scripts/WhaleMove.cs
+++ Assets/Scripts/WhaleMove.cs
@@ -7,7 +7,6 @@ public class WhaleMove : MonoBehaviour {
 	public Transform target;
 	public float moveSpeed = 5.0f;
 	public float rotationSpeed = 2.5f;
-	public float centeringDistance = 5f;
 
 	// Use this for initialization
 	void Start () {
@@ -21,13 +20,8 @@ public class WhaleMove : MonoBehaviour {
 
 	void MoveTowardsTarget() {
 		Vector3 direction = (target.position - transform.position);
-		Quaternion lookRotation;
-		if (direction.magnitude >= centeringDistance) {
-			lookRotation = Quaternion.LookRotation (direction);
-		} else {
-			lookRotation = target.rotation;
-		}
-		transform.rotation = Quaternion.Slerp (transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+		Quaternion lookRotation = Quaternion.LookRotation(direction);
+		transform.rotation = Quaternion.Slerp (transform.rotation, target.rotation, Time.deltaTime * rotationSpeed);
 		float step = moveSpeed * Time.deltaTime;
 		this.transform.position = Vector3.MoveTowards (transform.position, target.position, step);
 	}
