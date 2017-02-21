0dbbd4fd30ebffa5a198769540513e8e7c531888
Max Joseph Rais <rais.m@northeastern.edu>
2017-02-21 12:58:05 -0500

    the whale blows bubbles

---
+++ Assets/Scripts/Breathing.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Breathing : MonoBehaviour {
+
+	public Text bubbleText;
+	public Text breathText;
+	public float breathLength;
+	private float currentBreath;
+	private float bubbleFrequency;
+	private float minBubbleFrequency = 1;
+	public float waterHeight;
+	public GameObject bubblePrefab;
+	[Header("A float on the interval (0, 1]")]
+	// Closer to 0 is more bubbles
+	// Closer to 1 is less bubbles
+	public float bubbleFactor;
+
+	// Use this for initialization
+	void Start () {
+		currentBreath = breathLength;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// For testing
+		if (Input.GetKey("up")) {
+			gameObject.transform.position += (Vector3.up / 10);
+		}
+		else if (Input.GetKey("down")) {
+			gameObject.transform.position += (Vector3.down / 10);
+		}
+
+		// underwater
+		if(this.gameObject.transform.position.y < waterHeight) {
+			if (currentBreath <= 0) {
+				// out of breath
+			    Debug.Log ("Dead");
+				currentBreath = breathLength;
+				bubbleFrequency = bubbleFactor * currentBreath;
+			} 
+			else {
+				currentBreath -= Time.deltaTime;
+				if (bubbleFrequency <= 0) {
+					//spawn a bubble and reset bubbleFrequency
+					GameObject bubble = Instantiate(bubblePrefab) as GameObject;
+					bubble.transform.position = this.gameObject.transform.position + new Vector3 (-1, 1, 0);
+					bubbleFrequency = bubbleFactor * currentBreath;
+					if (bubbleFrequency < minBubbleFrequency) {
+						bubbleFrequency = minBubbleFrequency;
+					}
+				} else {
+					bubbleFrequency -= Time.deltaTime;
+				}
+			}
+		}
+		// above water - reset breath
+		else {
+			currentBreath = breathLength;
+			bubbleFrequency = bubbleFactor * currentBreath;
+		}
+
+		bubbleText.text = "Bubble: " + bubbleFrequency;
+		breathText.text = "Breath: " + currentBreath;
+	}
+}
---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour {
+
+	public float bubbleSpeed;
+	public float waterHeight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position += Vector3.up * bubbleSpeed * Time.deltaTime;
+		if (gameObject.transform.position.y >= waterHeight) {
+			Destroy (this.gameObject);
+		}
+	}
+}
