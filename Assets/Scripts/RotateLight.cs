732fcbe4bb82b865ea90dfabd6eda935bd5cc9d2
Spencer Franklin <franklin.sp@husky.neu.edu>
2017-03-02 15:22:29 -0500

    dank lighting

---
+++ Assets/Scripts/RotateLight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLight : MonoBehaviour {
+
+	public float rotSpeed = .75f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (0, 0, Time.deltaTime * rotSpeed);
+	}
+}
