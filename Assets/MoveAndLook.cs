0928b1e8706e4b24b5d5ac663583f126798d5310
glibjibb <glibjibb@gmail.com>
2017-03-02 15:32:06 -0500

    Overall Player Prefab
    Contains whale, camera, and movement center all in one prefab for easy
    drag-and-drop

---
+++ Assets/MoveAndLook.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAndLook : MonoBehaviour {
+
+	private Transform cameraPos;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		cameraPos = GameObject.FindGameObjectWithTag ("MainCamera").transform;
+	}
+
+	void FixedUpdate () {
+		transform.rotation = cameraPos.rotation;
+		transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+	}
+}
