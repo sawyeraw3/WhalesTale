8f012f58d9415594384442773371872ba6dfb06c
sp3nc3r <franklin.sp@husky.neu.edu>
2017-03-02 19:54:09 -0500

    Revert "fish"
    This reverts commit 405e42d52d1db9835707605285bbc1bd3870511b.

---
+++ Assets/Scripts/FishMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMove : MonoBehaviour {
+
+	public Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		direction = Vector3.forward;
+		transform.LookAt (direction);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		direction = Vector3.forward;
+		transform.LookAt (direction);
+	}
+}
