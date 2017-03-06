3efb6d78465d63fceb4ef647df7de831c9e1f465
sp3nc3r <franklin.sp@husky.neu.edu>
2017-03-06 10:16:09 -0500

    fixed lighting rotate speed

---
+++ Assets/Scripts/LightFollowPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFollowPlayer : MonoBehaviour {
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (player.transform.position.x + 10f, 0, player.transform.position.z + 10f);
+	}
+}
