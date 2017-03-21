27b49deecbad82e3812d538aad8f5ce3a916d5c5
Max Joseph Rais <rais.m@northeastern.edu>
2017-03-21 13:09:48 -0400

    fish spawning in game scene

---
+++ Assets/Scripts/FishHeadManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishHeadManager : MonoBehaviour {
+
+	public GameObject fishHeadPrefab;
+	public GameObject player;
+	private List<GameObject> fishHeads;
+	public float minDistance;
+	public int minHeads;
+
+	// Use this for initialization
+	void Start () {
+		fishHeads = new List<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int count = 0;
+		for(int i = 0; i < fishHeads.Count; i++) {
+			GameObject head = fishHeads [i];
+			if (!head) {
+				fishHeads.Remove (head);
+				i--;
+				continue;
+			}
+
+			// if its close enough to the whale
+			if (Mathf.Abs (Vector3.Distance (player.transform.position, head.transform.position)) < minDistance) {
+				count++;
+			} else {
+				fishHeads.Remove (head);
+				i--;
+				Destroy (head);
+			}
+		}
+
+		if (count < minHeads) {
+			Debug.Log ("making new");
+			GameObject head = Instantiate (fishHeadPrefab) as GameObject;
+			head.transform.position = player.transform.position;
+			fishHeads.Add (head);
+		}
+	}
+}
--- Assets/Scripts/FishHeadSpawn.cs
+++ Assets/Scripts/FishHeadSpawn.cs
@@ -23,15 +23,39 @@ public class FishHeadSpawn : MonoBehaviour {
 			GameObject fishObj = fish.GetComponentInChildren<MeshRenderer>().gameObject;
 			fishObj.transform.localPosition = spread;
 		}
+		dir = new Vector3 (Random.Range (-1, 1), Random.Range (-1, 1), Random.Range (-1, 1));
+		while (dir.x == 0) {
+			dir.Set (Random.Range (-1, 1), dir.y, dir.z);
+		}
+		while (dir.y == 0) {
+			dir.Set (dir.x, Random.Range (-1, 1), dir.z);
+		}
+		while (dir.z == 0) {
+			dir.Set (dir.x, dir.y, Random.Range (-1, 1));
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
+		int fishCount = 0;
 		foreach (GameObject fish in GameObject.FindGameObjectsWithTag("fishParent")) {
 			fish.transform.RotateAround (fish.transform.position, Vector3.up, Time.deltaTime * fishRotSpeed);
+			GameObject fishObj = fish.GetComponentInChildren<Transform> ().gameObject;
+			fishObj.transform.rotation = Quaternion.LookRotation (dir);
+
+			if (fish.transform.position.y > 0 || fish.transform.position.y < -100) {
+				Destroy (fish);
+			} else {
+				fishCount++;
+			}
 		}
 
-		this.transform.position = Vector3.MoveTowards (this.transform.position, this.transform.position + Vector3.forward, headMoveSpeed);
+		this.transform.position = Vector3.MoveTowards (this.transform.position, this.transform.position + dir, headMoveSpeed);
+
+		if (transform.position.y > 0 || transform.position.y < -100) {
+			if (fishCount == 0)
+				Destroy (this.gameObject);
+		}
 
 	}
 }
