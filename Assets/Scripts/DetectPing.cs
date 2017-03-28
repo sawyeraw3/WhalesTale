679e24233cc43f6d4e75157eeed58aea6c69f375
Spencer Franklin <franklin.sp@husky.neu.edu>
2017-03-28 12:31:41 -0400

    ping

---
+++ Assets/Scripts/DetectPing.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collision col){
+
+		if (col.gameObject.tag == "ping") {
+			Debug.Log ("anus");
+		}
+	}
+}
