7b56607400d2745944011976234f16e338782333
David Fasano <dfaz3@ccs.neu.edu>
2017-04-04 12:23:15 -0400

    new version and whale sounds on controller input

---
+++ Assets/Scripts/WhaleSounds.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleSounds : MonoBehaviour {
+
+	public AudioClip[] sounds;
+	private AudioSource whale;
+
+	// Use this for initialization
+	void Start () {
+		whale = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) || GvrController.ClickButtonUp && !whale.isPlaying) {
+			int index = Random.Range (0, sounds.Length);
+			whale.clip = sounds [index];
+			whale.Play ();
+		}
+	}
+}
