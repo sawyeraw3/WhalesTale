f256d84c14a3dad20958acade2f3688f886752f9
Spencer Franklin <franklin.sp@husky.neu.edu>
2017-04-06 16:25:45 -0400

    script

---
+++ Assets/Scripts/MoveToBottom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToBottom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		StartCoroutine (cast());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	IEnumerator cast(){
+		yield return new WaitForSeconds (.5f);
+		Random.seed = System.DateTime.Now.Millisecond;
+		transform.rotation = Random.rotation;
+		RaycastHit hit;
+		Ray ray = new Ray (transform.position, Vector3.down);
+		if (Physics.Raycast (ray, out hit, Mathf.Infinity)) {
+			if (hit.collider.tag == "floor") {
+				this.transform.position = hit.point;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/SpawnOceanFloor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOceanFloor : MonoBehaviour {
+
+	public GameObject[] Rocks;
+	private GameObject[] spawnRocks;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 20; i++) {
+			Random.seed = System.DateTime.Now.Millisecond;
+			GameObject rock = Instantiate (Rocks [Random.Range (0, Rocks.Length - 1)]) as GameObject;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
