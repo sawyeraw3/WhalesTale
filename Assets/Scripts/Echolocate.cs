4929e303179f49e42c1b3b149470fa97b1a5fae0
David Fasano <dfaz3@ccs.neu.edu>
2017-04-10 19:07:47 -0400

    Render When Near script & ping changes

--- Assets/Scripts/Echolocate.cs
+++ Assets/Scripts/Echolocate.cs
@@ -7,14 +7,17 @@ public class Echolocate : MonoBehaviour {
 	public float radius;
 	public float expandSpeed;
 	public GameObject indicator;
+	public Transform playerLoc;
 
 	// Use this for initialization
 	void Start () {
 		radius *= 10000;
+		playerLoc = GameObject.FindGameObjectWithTag ("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		this.transform.position = playerLoc.position;
 		this.transform.localScale += new Vector3 (expandSpeed, expandSpeed, expandSpeed); 
 		if (this.transform.lossyScale.magnitude >= radius)
 			DestroyImmediate (this.gameObject);
---
+++ Assets/Scripts/RenderWhenNear.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderWhenNear : MonoBehaviour {
+
+	private bool isEnabled = false;
+	private Transform playerLoc;
+	// Use this for initialization
+	void Start () {
+		playerLoc = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+	
+	void FixedUpdate() {
+		if (Vector3.Distance (this.transform.position, playerLoc.position) < 110)
+			isEnabled = true;
+		else
+			isEnabled = false;
+	}
+
+	void Update () {
+		if (isEnabled) {
+			this.GetComponentInChildren<Renderer> ().enabled = true;
+		} else
+			this.GetComponentInChildren<Renderer> ().enabled = false;
+	}
+}
