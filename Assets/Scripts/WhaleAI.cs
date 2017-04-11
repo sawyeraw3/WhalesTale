854d00e8fed41eba015bddb0a2ebcd29185c2de9
Spencer Franklin <franklin.sp@husky.neu.edu>
2017-04-11 13:21:38 -0400

    woop

--- Assets/Scripts/WhaleAI.cs
+++ Assets/Scripts/WhaleAI.cs
@@ -5,16 +5,47 @@ using UnityEngine;
 public class WhaleAI : MonoBehaviour {
 
 	public GameObject player;
+	public float rotSpeed;
+	public float moveSpeed;
+	public float randWait;
+	public float MaxDisFromWhale;
+	Vector3 buffer;
 
 	// Use this for initialization
 	void Start () {
-		
+		newRandWait();
+		MaxDisFromWhale = 10f;//replace with number of whales in pod
+		buffer = Random.insideUnitSphere * MaxDisFromWhale;
+		StartCoroutine (newPos());
+		rotSpeed = .5f;
+		moveSpeed = 10f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
 		transform.rotation = Quaternion.Slerp (transform.rotation, 
-			Quaternion.LookRotation (player.transform.position - transform.position), 
-			Time.fixedDeltaTime);
+			Quaternion.LookRotation (player.transform.position + buffer - transform.position), 
+			rotSpeed * Time.fixedDeltaTime);
+		transform.Translate (Vector3.forward * moveSpeed * Time.fixedDeltaTime);
 	}
+
+	IEnumerator newPos(){
+		while (true) {
+			newRandWait ();
+			yield return new WaitForSeconds (randWait);
+			Random.seed = System.DateTime.Now.Millisecond;
+			//min and max move speed
+			moveSpeed = Random.Range (10, 13);
+			buffer = Random.insideUnitSphere * MaxDisFromWhale;
+
+		}
+
+	}
+
+	void newRandWait(){
+		Random.seed = System.DateTime.Now.Millisecond;
+		//min and max wait time for new position
+		randWait = Random.Range (5f, 10f);
+	}
+		
 }
