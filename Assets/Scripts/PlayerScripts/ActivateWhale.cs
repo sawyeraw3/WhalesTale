2b7a4d1140c59e56c8be04fbbcc18506600c06b6
David Fasano <dfaz3@ccs.neu.edu>
2017-04-10 21:16:48 -0400

    Added "Debugging" mode to player script
    This deactivates the intro sequence so that you can get right into
    testing

--- Assets/Scripts/PlayerScripts/ActivateWhale.cs
+++ Assets/Scripts/PlayerScripts/ActivateWhale.cs
@@ -8,21 +8,29 @@ public class ActivateWhale : MonoBehaviour {
 	private Animator camAnim;
 	private GameObject gameCamera;
 	private GameObject player;
+	public bool debugging = false;
 	// Use this for initialization
 	void Start () {
 		gameCamera = GameObject.Find ("FreeLookCameraRig");
 		camAnim = gameCamera.GetComponent<Animator> ();
 		player = GameObject.Find ("whale");
+		if (debugging) {
+			Activate ();
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (camAnim.GetCurrentAnimatorStateInfo (0).normalizedTime > 1 && !camAnim.IsInTransition (0))
+		if (!debugging && camAnim.GetCurrentAnimatorStateInfo (0).normalizedTime > 1 && !camAnim.IsInTransition (0))
 		{
-			camAnim.enabled = false;
-			gameCamera.GetComponent<FreeLookCam> ().enabled = true;
-			gameCamera.GetComponent<ProtectCameraFromWallClip> ().enabled = true;
-			player.GetComponent<WhaleControl> ().enabled = true;
+			Activate ();
 		}
+	}
+
+	void Activate() {
+		camAnim.enabled = false;
+		gameCamera.GetComponent<FreeLookCam> ().enabled = true;
+		gameCamera.GetComponent<ProtectCameraFromWallClip> ().enabled = true;
+		player.GetComponent<WhaleControl> ().enabled = true;
 	}
 }
