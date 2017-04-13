22fc586fc8a9ddb4e7c0b54689f741f3a0026f98
David Fasano <dfaz3@ccs.neu.edu>
2017-04-13 18:27:31 -0400

    Handles zooming out better
    Zooms out at a lower interval, zooms back in on player if they breach

--- Assets/Scripts/WhalesInPod.cs
+++ Assets/Scripts/WhalesInPod.cs
@@ -6,36 +6,51 @@ public class WhalesInPod : MonoBehaviour {
 
 	public int podCount;
 	private GameObject followPoint;
+	private Transform playerPos;
 	// Use this for initialization
 	void Start () {
 		podCount = 0;
 		followPoint = GameObject.Find ("Focus");
+		playerPos = GameObject.FindGameObjectWithTag ("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		switch (podCount) {
-		case 0:
-			followPoint.transform.localPosition = new Vector3 (0, 0, 0);
-			break;
-		case 1: 
-			followPoint.transform.localPosition = new Vector3 (0, 0, -0.1f);
-			break;
-		case 2: 
-			followPoint.transform.localPosition = new Vector3 (0, 0, -0.2f);
-			break;
-		case 3: 
-			followPoint.transform.localPosition = new Vector3 (0, 0, -0.3f);
-			break;
-		case 4: 
-			followPoint.transform.localPosition = new Vector3 (0, 0, -0.4f);
-			break;
-		case 5: 
-			followPoint.transform.localPosition = new Vector3 (0, 0, -0.5f);
-			break;
-		default: 
-			followPoint.transform.localPosition = new Vector3 (0, 0, -1f);
-			break;
+		if (playerPos.position.y > 0)
+			followPoint.transform.localPosition = Vector3.zero;
+		else {
+			switch (podCount) {
+			case 0:
+				followPoint.transform.localPosition = Vector3.zero;
+				break;
+			case 1: 
+				followPoint.transform.localPosition = new Vector3 (0, 0, -0.05f);
+				break;
+			case 2: 
+				followPoint.transform.localPosition = new Vector3 (0, 0, -0.1f);
+				break;
+			case 3: 
+				followPoint.transform.localPosition = new Vector3 (0, 0, -0.15f);
+				break;
+			case 4: 
+				followPoint.transform.localPosition = new Vector3 (0, 0, -0.2f);
+				break;
+			case 5: 
+				followPoint.transform.localPosition = new Vector3 (0, 0, -0.25f);
+				break;
+			case 6: 
+				followPoint.transform.localPosition = new Vector3 (0, 0, -0.3f);
+				break;
+			case 7: 
+				followPoint.transform.localPosition = new Vector3 (0, 0, -0.35f);
+				break;
+			case 8: 
+				followPoint.transform.localPosition = new Vector3 (0, 0, -0.4f);
+				break;
+			default: 
+				followPoint.transform.localPosition = new Vector3 (0, 0, -1f);
+				break;
+			}
 		}
 	}
 }
