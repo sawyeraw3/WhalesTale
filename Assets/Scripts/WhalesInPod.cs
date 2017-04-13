56c52fc22ce034b89cdcb67753dafd8b408ed20a
David Fasano <dfaz3@ccs.neu.edu>
2017-04-13 18:14:02 -0400

    Fix layer issue and script for zooming camera out

--- Assets/Scripts/WhalesInPod.cs
+++ Assets/Scripts/WhalesInPod.cs
@@ -5,13 +5,37 @@ using UnityEngine;
 public class WhalesInPod : MonoBehaviour {
 
 	public int podCount;
+	private GameObject followPoint;
 	// Use this for initialization
 	void Start () {
 		podCount = 0;
+		followPoint = GameObject.Find ("Focus");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		switch (podCount) {
+		case 0:
+			followPoint.transform.localPosition = new Vector3 (0, 0, 0);
+			break;
+		case 1: 
+			followPoint.transform.localPosition = new Vector3 (0, 0, -0.1f);
+			break;
+		case 2: 
+			followPoint.transform.localPosition = new Vector3 (0, 0, -0.2f);
+			break;
+		case 3: 
+			followPoint.transform.localPosition = new Vector3 (0, 0, -0.3f);
+			break;
+		case 4: 
+			followPoint.transform.localPosition = new Vector3 (0, 0, -0.4f);
+			break;
+		case 5: 
+			followPoint.transform.localPosition = new Vector3 (0, 0, -0.5f);
+			break;
+		default: 
+			followPoint.transform.localPosition = new Vector3 (0, 0, -1f);
+			break;
+		}
 	}
 }
