a1b2004671573ab4aff2b3ecd83c1e89cd50822b
SpencerFranklin <franklin.sp@husky.neu.edu>
2017-04-17 12:34:11 -0400

    6 new plants

---
+++ Assets/Scripts/RandomMaterialGroup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMaterialGroup : MonoBehaviour {
+
+	public List<Material> materials = new List<Material>();
+	private List<Material> backup = new List<Material>();
+	private GameObject child;
+	// Use this for initialization
+	void Start () {
+		Random.seed = System.DateTime.Now.Millisecond;
+
+		for (int i = 0; i < this.transform.childCount ; i++){
+			child = this.gameObject.transform.GetChild(i).gameObject;
+
+			if (materials.Count == 0){
+				materials = new List<Material>(backup);
+				backup.RemoveRange(0, backup.Count);
+			}
+			int index = Random.Range(0, materials.Count - 1);
+			child.GetComponent<Renderer>().material = materials[index];
+			backup.Add(materials[index]);
+			materials.RemoveAt(index);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
