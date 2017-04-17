09a6fe9ff7bdec14debf2cdfb173e7afcf8f8218
SpencerFranklin <franklin.sp@husky.neu.edu>
2017-04-16 20:13:11 -0400

    prefabs

---
+++ Assets/Scripts/RandomMaterial.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMaterial : MonoBehaviour {
+
+	public Material[] materials;
+
+	// Use this for initialization
+	void Start () {
+		Random.seed = System.DateTime.Now.Millisecond;
+
+		this.GetComponent<Renderer>().material = materials[Random.Range(0, materials.Length)];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
