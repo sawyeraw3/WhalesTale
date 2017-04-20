87096305c328d448f12a6eed2e86d02fa42c3bae
SpencerFranklin <franklin.sp@husky.neu.edu>
2017-04-19 20:30:37 -0400

    attempt

--- Assets/Scripts/GenerateInfinite.cs
+++ Assets/Scripts/GenerateInfinite.cs
@@ -14,6 +14,11 @@ public class GenerateInfinite : MonoBehaviour {
 	[Range(0,1)]
 	public float smallObjectsChance = 0.3f;
 	public GameObject[] smallPrefabs;
+	private int spawnCounter = 0; 
+
+	private GameObject[] smallPrefabPool = new GameObject[27 * 15];
+	private List<GameObject> objList, deactiveObj, activeObj;
+
 
 	int seed;
 
@@ -41,6 +46,20 @@ public class GenerateInfinite : MonoBehaviour {
 	}
 
 	void Start() {
+		objList = new List<GameObject>(smallPrefabs);
+		for (int i = 0; i < smallPrefabPool.Length - 1; i++){
+			int j = Random.Range(0, objList.Count);
+			smallPrefabPool[i] = Instantiate (objList[j]) as GameObject;
+			objList.RemoveAt(j);
+
+			if (objList.Count == 0){
+				objList = new List<GameObject>(smallPrefabs);
+			}
+				
+		}
+		deactiveObj = new List<GameObject>(smallPrefabPool);
+
+
 		seed = 100;
 
 		heightScale = plane.GetComponent<GenerateTerrain> ().heightScale;
@@ -57,7 +76,7 @@ public class GenerateInfinite : MonoBehaviour {
 				GameObject t = (GameObject) Instantiate(plane, pos, Quaternion.identity);
 				t.GetComponent<GenerateTerrain> ().perlinMesh (seed);
 
-				SpawnSmallObjects (t);
+				SpawnSmallObjects2 (t);
 
 				string tilename = "Tile_" + ((int)(pos.x)).ToString() + "_" + ((int)(pos.z)).ToString();
 				t.name = tilename;
@@ -92,7 +111,7 @@ public class GenerateInfinite : MonoBehaviour {
 
 						t.GetComponent<GenerateTerrain> ().perlinMesh (seed);
 
-						SpawnSmallObjects (t);
+						SpawnSmallObjects2 (t);
 
 						t.name = tilename;
 						Tile tile = new Tile(t, updateTime);
@@ -108,8 +127,19 @@ public class GenerateInfinite : MonoBehaviour {
 			//destroy all tiles not just created or with time updated
 			//and put new tiles and tiles to be kept in a new hastable
 			Hashtable newTerrain = new Hashtable();
+			GameObject tmp;
 			foreach (Tile tls in tiles.Values) {
 				if (tls.creationTime != updateTime) {
+					tmp = tls.theTile.transform.GetChild(0).gameObject;
+
+					Renderer rend = tmp.GetComponent<Renderer>();
+					rend.enabled = false;
+					tmp.transform.localScale = new Vector3(1,1,1);
+					tmp.transform.parent = null;
+					int remI = activeObj.IndexOf(tmp);
+					deactiveObj.Add(tmp);
+					activeObj.RemoveAt(remI);
+
 					Destroy(tls.theTile);
 				}
 				else {
@@ -148,5 +178,40 @@ public class GenerateInfinite : MonoBehaviour {
 		}
 
 	}
+
+	void SpawnSmallObjects2(GameObject tile)
+	{
+		Mesh mesh = tile.GetComponent<MeshFilter>().mesh;
+		Vector3[] vertices = mesh.vertices;
+		Vector3 minV = vertices [0];
+		Vector3 maxV = vertices [vertices.Length - 1];
+
+		float objectHere = Random.value;
+
+		if(objectHere < smallObjectsChance) {
+			int findSpot = Random.Range (0, vertices.Length);
+			Vector3 randSpot = vertices [findSpot];
+			int randObj = Random.Range (0, smallPrefabs.Length);
+			float randSize = Random.Range (0.25f, maxSmallSize);
+			int chooseVertice = Random.Range (0, vertices.Length);
+			Vector3 meshPos = vertices [chooseVertice];
+			Vector3 spawnPos = tile.transform.position;
+
+			int i = Random.Range(0, deactiveObj.Count -1);
+			GameObject temp = deactiveObj[i];
+			temp.transform.position = spawnPos;
+			temp.transform.rotation = new Quaternion(0,0,0,0);
+			temp.transform.parent = tile.transform;
+			temp.transform.localPosition = meshPos;
+			temp.transform.localScale *= randSize;
+			Renderer rend = temp.GetComponent<Renderer>();
+			rend.enabled = true;
+			activeObj.Add(temp);
+			deactiveObj.RemoveAt(i);
+
+		}
+
+	}
+
 		
 }
